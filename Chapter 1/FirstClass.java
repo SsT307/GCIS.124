e0015b6ae122be96264b058ca17181cde32a0dea
SsT307 <shaik.alhajri22@gmail.com>
2025-01-13 16:17:19 +0400

    first commit

---
+++ Chapter 1/FirstClass.java
@@ -0,0 +1,8 @@
+// The file has to have the same name as the class, you can create INNER classes (class inside another class) only
+public class FirstClass { 
+    // Main Method:
+    public static void main (String [] args) 
+    {
+        System.out.println("Hello World!"); // PRINTING A MSG
+    }
+}
